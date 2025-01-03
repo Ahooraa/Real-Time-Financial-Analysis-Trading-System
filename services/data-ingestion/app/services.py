9d19ebbf4a217fcb659f98fb94b7d3bce2154356
Ahoora <ahoora.amini8@gmail.com>
2025-01-03 12:33:33 +0330

    sends data to seperate kafka topics

--- services/data-ingestion/app/services.py
+++ services/data-ingestion/app/services.py
@@ -1,25 +1,29 @@
 from app.kafka_producer import send_to_kafka
 import logging
+
 logging.basicConfig(level=logging.INFO)
 
 def process_data(data):
-    # Validate required fields
-    # required_fields = ["stock_symbol", "opening_price", "closing_price", "timestamp"]
-    # for field in required_fields:
-    #     if field not in data:
-    #         return {"error": f"Missing field: {field}"}
-
-    # Ensure numerical fields are valid
-    # try:
-    #     if(["average_price"] in data):
-    #         data["average_price"] = (float(data["opening_price"]) + float(data["closing_price"])) / 2
-    # except ValueError:
-    #     return {"error": "Invalid numerical values for opening or closing price"}
+    # Determine the topic based on the data type
+    if "data_type" in data:
+        data_type = data["data_type"]
+        if data_type == "order_book":
+            topic = "order_book"
+        elif data_type == "news_sentiment":
+            topic = "news_sentiment"
+        elif data_type == "market_data":
+            topic = "market_data"
+        elif data_type == "economic_indicator":
+            topic = "economic_indicator"
+        else:
+            return {"error": "Unknown data type"}
+    else:
+        topic = "stock_prices"
 
     # Forward to Kafka
-    logging.info("received message from the generator: ", data)
-    kafka_result = send_to_kafka(data)
+    logging.info("received message from the generator: %s", data)
+    kafka_result = send_to_kafka(data, topic)
     if not kafka_result:
         return {"error": "Failed to send data to Kafka"}
 
-    return {"status": "success", "message": "Data processed and forwarded to Kafka", "data": data}
+    return {"status": "success", "message": f"Data processed and forwarded to Kafka topic:{topic}", "data": data}
