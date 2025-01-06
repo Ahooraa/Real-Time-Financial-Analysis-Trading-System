11fac37009fdeb1dd5907d307421f870824798f7
Ahoora <ahoora.amini8@gmail.com>
2025-01-07 01:47:59 +0330

    Dynamic topic mapping + better error handling

--- services/data-ingestion/app/services.py
+++ services/data-ingestion/app/services.py
@@ -5,24 +5,20 @@ from zoneinfo import ZoneInfo
 
 logging.basicConfig(level=logging.INFO)
 
-# Mapping of stock symbols to topics
-symbol_to_topic = {
-    "BTCIRT": "btcirt_topic",
-    "USDTIRT": "usdtirt_topic",
-    "ETHIRT": "ethirt_topic",
-    "ETCIRT": "etcirt_topic",
-    "SHIBIRT": "shibirt_topic"
-}
-
 def process_data(data):
-    # Determine the topic based on the stock symbol
-    stock_symbol = data.get("stock_symbol", "")
-    topic = symbol_to_topic.get(stock_symbol, "default_topic")
+    try:
+        # Determine the topic based on the stock symbol
+        stock_symbol = data.get("stock_symbol", "")
+        topic = f"{stock_symbol.lower()}_topic" if stock_symbol else "default_topic"
 
-    # Forward to Kafka
-    logging.info("received message from the generator: %s", data)
-    kafka_result = send_to_kafka(data, topic, key=stock_symbol)
-    if not kafka_result:
-        return {"error": "Failed to send data to Kafka"}
+        # Forward to Kafka
+        logging.info("received message from the API: %s", data)
+        kafka_result = send_to_kafka(data, topic, key=stock_symbol)
+        if not kafka_result:
+            return {"error": "Failed to send data to Kafka"}
 
-    return {"status": "success", "message": f"Data processed and forwarded to Kafka topic:{topic}", "data": data}
+        return {"status": "success", "message": f"Data processed and forwarded to Kafka topic:{topic}", "data": data}
+    
+    except Exception as e:
+        logging.error("Exception occurred while processing data: %s", str(e))
+        return {"error": "Exception occurred while processing data", "details": str(e)}
