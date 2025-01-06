8a5fae379e28cb118d76cb1deb6c94ec581c29cd
Ahoora <ahoora.amini8@gmail.com>
2025-01-07 01:49:19 +0330

    changed terminal output + lcoal_time

--- api.py
+++ api.py
@@ -48,7 +48,8 @@ try:
                     latest_index = -1  # Last element
                     payload = {
                         "stock_symbol": symbol,
-                        "timestamp": format_timestamp(timestamps[latest_index]),
+                        "timestamp": timestamps,
+                        "local_time":format_timestamp(timestamps[latest_index]),
                         "open": open_prices[latest_index],
                         "high": high_prices[latest_index],
                         "low": low_prices[latest_index],
@@ -59,7 +60,8 @@ try:
                     # Send the data to the Flask app for ingestion
                     flask_response = requests.post(FLASK_API_URL, json=payload)
                     if flask_response.status_code == 200:
-                        print("Data sent to Flask app successfully.")
+                        flask_response_json = flask_response.json()
+                        print(flask_response_json.get("message"),"\n", f"data: {flask_response_json.get('data')}\n\n")
                     else:
                         print(f"Failed to send data to Flask app: {flask_response.status_code}")
 
